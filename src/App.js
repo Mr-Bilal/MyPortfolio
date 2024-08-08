d7a89c1a0c4ffea615a0f79e54207b8020ae4e57
Mr-Bilal <bilal.mani6011@gmail.com>
2024-08-08 15:49:16 +0500

    gives exact path of main page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { createBrowserRouter , RouterProvider } from 'react-router-dom';
 const App = () => {
   const router = createBrowserRouter([
     {
-      path : "/",
+      path : "/about",
       element : <><Navbar /><About /></>
     },
     {
